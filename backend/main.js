484ef88a1b8c0c1718380e710c801074bec1a2d0
Aagamjain64 <aagamjain0604@gmail.com>
2025-07-26 13:15:20 +0530

    some changes

--- backend/main.js
+++ backend/main.js
@@ -9,7 +9,7 @@ const app = express();
 
 // ✅ Apply CORS middleware FIRST
 app.use(cors({
-  origin: 'https://jan-samasya.netlify.app',
+  origin: 'http://localhost:5173',
   credentials: true
 }));
 
